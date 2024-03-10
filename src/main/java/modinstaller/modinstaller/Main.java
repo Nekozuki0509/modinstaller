4f00d477adaab880908fcd6ad277e777cde3ec1e
Nekouzki <106313126+Nekozuki2009@users.noreply.github.com>
2024-03-10 22:32:10 +0900

    fin

--- src/main/java/modinstaller/modinstaller/Main.java
+++ src/main/java/modinstaller/modinstaller/Main.java
@@ -3,7 +3,6 @@ package modinstaller.modinstaller;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import javax.net.ssl.HttpsURLConnection;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -34,56 +33,33 @@ public class Main {
         JsonNode json = mapper.readTree(new File("config.json"));
         String version = json.get("version").asText();
         for (JsonNode id : json.get("ids")){
-            System.out.println(id.asText());
             HttpURLConnection con = (HttpURLConnection) new URL("https://api.modrinth.com/v2/project/"+id.asText()
-                    +"/version?loaders=[fabric]&game_versions=["+version+"]").openConnection();
+                    +"/version?loaders=[%22fabric%22]&game_versions=[%22"+version+"%22]").openConnection();
+
             con.setRequestMethod("GET");
-            con.getResponseCode();
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+            BufferedReader in;
+            try {
+                in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } catch (FileNotFoundException e){
+                System.out.println("Could not find any mod of id:"+id.asText());
+                continue;
+            }
+
             String inputLine;
             StringBuffer response = new StringBuffer();
             while ((inputLine = in.readLine()) != null) {
                 response.append(inputLine);
             }
             in.close();
-            String url;
-            if ((url=mapper.readTree(response.toString()).get("files").get(0).get("url").asText()) == null) {
-                HttpURLConnection con2 = (HttpURLConnection) new URL("https://api.modrinth.com/v2/project/"+id).openConnection();
-                con2.setRequestMethod("GET");
-                con2.getResponseCode();
-                BufferedReader in2 = new BufferedReader(new InputStreamReader(con2.getInputStream()));
-                String inputLine2;
-                StringBuffer response2 = new StringBuffer();
-                while ((inputLine2 = in2.readLine()) != null) {
-                    response2.append(inputLine2);
-                }
-                in2.close();
-                String title;
-                if ((title=mapper.readTree(response2.toString()).get("title").asText()) == null) {
-                    System.out.println("Could not find any mod of id:"+id);
-                } else {
-                    System.out.println("Could not find "+title+" of version:"+version);
+            JsonNode res = mapper.readTree(response.toString()).get(0);JsonNode files;
+            Path file_p;
+            if (Files.notExists(file_p=mods.resolve((files=res.get("files").get(0)).get("filename").asText()))) {
+                try (InputStream downin = new URL(files.get("url").asText()).openStream()) {
+                    Files.copy(downin, file_p);
                 }
             } else {
-
-                // HTTPS接続を開く
-                HttpsURLConnection conn = (HttpsURLConnection) new URL(url).openConnection();
-
-                // ファイルをダウンロード
-                try (BufferedInputStream downin = new BufferedInputStream(conn.getInputStream());
-                     FileOutputStream fileOutputStream = new FileOutputStream("mods")) {
-                    byte[] dataBuffer = new byte[1024];
-                    int bytesRead;
-                    while ((bytesRead = downin.read()) != -1) {
-                        fileOutputStream.write(dataBuffer, 0, bytesRead);
-                    }
-                    System.out.println("fin");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                // 接続を閉じる
-                conn.disconnect();
+                System.out.println(files.get("filename").asText() + " is already exists");
             }
         }
     }
